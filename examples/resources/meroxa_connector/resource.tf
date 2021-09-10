0228111639eca3f85d72f5bd4213f09ca662c47f
Raúl Barroso <ra.barroso@gmail.com>
2021-09-10 16:48:28 +0200

    doc: update example
    Co-authored-by: TJ Leonard <tj@meroxa.io>

--- examples/resources/meroxa_connector/resource.tf
+++ examples/resources/meroxa_connector/resource.tf
@@ -4,8 +4,13 @@ resource "meroxa_resource" "inline" {
   url  = "postgres://foo:bar@example:5432/db"
 }
 
+resource "meroxa_pipeline" "basic" {
+  name = "pipeline"
+}
+
 resource "meroxa_connector" "basic" {
-  name      = "basic"
-  source_id = meroxa_resource.inline.id
-  input     = "public.Users"
+  name        = "basic"
+  source_id   = meroxa_resource.inline.id
+  input       = "public.Users"
+  pipeline_id = meroxa_pipeline.basic.id
 }
